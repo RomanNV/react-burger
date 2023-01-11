917cc1b46bc94748ca153b48baa07e82439bcd90
RomanNV <romanosow@mail.ru>
2023-01-12 02:11:03 +0300

    добавлены 2 компонента и закончена верстка главной страницы

--- src/components/BurgerIngredients/BurgerIngredients.jsx
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -1,13 +1,28 @@
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
-import bun from "../../images/bun-02.svg";
-import sauce from "../../images/sauce-03.png";
-import meate from "../../images/meat-02.png";
-import sp from "../../images/sp 1.png";
-import mineralRing from "../../images/mineral rings.png";
 import styles from "./BurgerIngredients.module.css";
 import { data } from "../../utils/data.js";
+import { BurgerGroup } from "../BurgerGroup/BurgerGroup";
+import PropTypes from "prop-types";
 
-export const BurgerIngredients = () => {
+export const BurgerIngredients = ({ dataIngredients }) => {
+  const isCounter = true;
+  const tabDataBun = data.filter((element) => {
+    if (element.type === "bun") {
+      return element;
+    }
+  });
+
+  const tabDataMain = data.filter((element) => {
+    if (element.type === "main") {
+      return element;
+    }
+  });
+  const tabDataSauce = data.filter((element) => {
+    if (element.type === "sauce") {
+      return element;
+    }
+  });
+
   return (
     <section className={styles.content_box}>
       <h1 className={`text text_type_main-large`}>Соберите бургер</h1>
@@ -16,9 +31,33 @@ export const BurgerIngredients = () => {
         <Tab>Соусы</Tab>
         <Tab>Начинки</Tab>
       </div>
-      <ul>
-        <li></li>
+
+      <ul className={`custom-scroll ${styles.ul_box}`}>
+        <li>
+          <BurgerGroup
+            tabData={tabDataBun}
+            title={"Булки"}
+            isCounter={isCounter}
+          ></BurgerGroup>
+        </li>
+        <li>
+          <BurgerGroup
+            tabData={tabDataSauce}
+            title={"Соусы"}
+            isCounter={isCounter}
+          ></BurgerGroup>
+        </li>
+        <li>
+          <BurgerGroup
+            tabData={tabDataMain}
+            title={"Начинки"}
+            isCounter={isCounter}
+          ></BurgerGroup>
+        </li>
       </ul>
     </section>
   );
+};
+BurgerIngredients.propTypes = {
+  dataIngredients: PropTypes.array,
 };
