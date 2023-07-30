eb38c41593e53daad91f25b8911bf052cb8ef861
Roman Nosov <85586036+RomanNV@users.noreply.github.com>
2023-07-30 15:42:04 +0300

    добавлен cypress тест конструктора

---
+++ aa.js
@@ -0,0 +1,65 @@
+{
+    "name": "react-burger",
+    "version": "0.1.0",
+    "private": true,
+    "dependencies": {
+      "@reduxjs/toolkit": "^1.9.3",
+      "@testing-library/jest-dom": "^5.16.5",
+      "@testing-library/user-event": "^13.5.0",
+      "@types/jest": "^27.5.2",
+      "@types/node": "^16.18.11",
+      "@types/react": "^18.0.26",
+      "@types/react-dom": "^18.0.10",
+      "@ya.praktikum/react-developer-burger-ui-components": "1.14.0",
+      "immutability-helper": "^3.1.1",
+      "localforage": "^1.10.0",
+      "match-sorter": "^6.3.1",
+      "prop-types": "^15.8.1",
+      "react": "^18.2.0",
+      "react-dnd": "^16.0.1",
+      "react-dnd-html5-backend": "^16.0.1",
+      "react-dom": "^18.2.0",
+      "react-loader-spinner": "^5.3.4",
+      "react-redux": "^8.0.5",
+      "react-router-dom": "^6.8.1",
+      "react-scripts": "5.0.1",
+      "redux": "^4.2.1",
+      "redux-devtools-extension": "^2.13.9",
+      "redux-thunk": "^2.4.2",
+      "sort-by": "^1.2.0",
+      "typescript": "^4.9.4",
+      "web-vitals": "^2.1.4"
+    },
+    "scripts": {
+      "cypress:open": "./node_modules/.bin/cypress open",
+      "start": "react-scripts start",
+      "build": "react-scripts build",
+      "test": "react-scripts test",
+      "eject": "react-scripts eject"
+    },
+    "eslintConfig": {
+      "extends": [
+        "plugin:cypress/recommended",
+        "react-app",
+        "react-app/jest"
+      ]
+    },
+    "browserslist": {
+      "production": [
+        ">0.2%",
+        "not dead",
+        "not op_mini all"
+      ],
+      "development": [
+        "last 1 chrome version",
+        "last 1 firefox version",
+        "last 1 safari version"
+      ]
+    },
+    "devDependencies": {
+      "cypress": "^12.17.2",
+      "eslint-plugin-cypress": "^2.13.3",
+      "gh-pages": "^5.0.0"
+    }
+  }
+  
--- cypress.config.ts
+++ cypress.config.ts
@@ -8,14 +8,8 @@ export default defineConfig({
     },
   },
 
-  component: {
-    devServer: {
-      framework: "create-react-app",
-      bundler: "webpack",
-    },
-  },
-
   e2e: {
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
